a6940cacfdcf0d0ad29fd249594a1e16ba0f0917
Ig Momin Khan <mominkhan8991@gmail.com>
2023-05-05 05:36:53 +0300

    blog posts data added

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-const chefs = require('./public/data.json')
-
+const chefs = require("./public/data.json");
 
 // static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+// cross-origin access
+app.use(cors());
 
 // handle get requiest to the home route '/'
-app.get('/',(req,res)=> {
-  res.send(`Server is running on port ${port}`)
+app.get("/", (req, res) => {
+  res.send(`Server is running on port ${port}`);
 });
 
-
-// handle get requiest for each chef by their unique id
-app
-
-
-
 // handle get request at chefs route
-app.get('/chefs',(req,res)=> {
+app.get("/chefs", (req, res) => {
   res.send(chefs);
 });
 
+// handle get request at
+app.get("/chef/:id", (req, res) => {
+  const reqId = req.params.id;
+  const chef = chefs.find((chef) => chef._id === reqId);
+  res.send(chef);
+});
 
-// handle get request at 
-app.get('/chef/:id',(req,res)=> {
-const reqId = req.params.id;
-  const chef = chefs.find(chef => chef._id === reqId)
-res.send(chef);
-})
-
-
-
-
-
-
-
+// return blog posts
+app.get("/posts", (req, res) => {
+  res.send(require("./public/posts.json"));
+});
 
 // listen on PORT || 5000
-app.listen(port,()=> console.log(`server is running on port ${port}`))
+app.listen(port, () => console.log(`server is running on port ${port}`));
